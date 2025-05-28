e90a01e009464c944d5c4a124fdf6eaac677fab0
TalhaAhad <mrsq3411@gmail.com>
2025-05-28 14:58:58 -0700

    Home page are left and faqs and sitmap are redy

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,4 @@ $(document).ready(function(){
   $(".bar").click(function(){
     $(this).find(".answer").slideToggle();
   });
-  $(".bar").click(function(){
-    $(this).find(".i").toggleClass("fa-plus fa-minus");
-  });
 });
