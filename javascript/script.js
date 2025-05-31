5cbf69d2fbfd6c60ccd58bb6ac749e80dc386592
TalhaAhad <mrsq3411@gmail.com>
2025-05-31 14:54:50 -0700

    home page is now responsive

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,28 @@ $(document).ready(function(){
     $(this).find(".answer").slideToggle();
   });
 });
+
+// hambuger menu click function
+$(document).ready(function() {
+    const $hamburgerMenu = $('#hamburgerMenu');
+    const $navbarLinks = $('#navbarLinks');
+
+    // Toggle the 'active' class on click
+    $hamburgerMenu.on('click', function() {
+        $navbarLinks.toggleClass('active');
+    });
+
+    // and only on small screens
+    $navbarLinks.find('a').on('click', function() {
+        if ($(window).width() <= 768) { // Check viewport width using jQuery
+            $navbarLinks.removeClass('active');
+        }
+    });
+});
+
+// hamburger Animation 
+$(document).ready(function() {
+  $("#hamburgerMenu").click(function(){
+    $("#navbarLinks").slideToggle(300);
+  })
+});
