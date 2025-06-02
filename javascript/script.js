da1fd85386aa082bc9c39e57186ce9d8c3f463c3
TalhaAhad <mrsq3411@gmail.com>
2025-06-02 04:55:47 -0700

    nothing

--- javascript/script.js
+++ javascript/script.js
@@ -32,3 +32,28 @@ $(document).ready(function() {
     $("#navbarLinks").slideToggle(300);
   })
 });
+
+
+
+// Ensure dropdown is toggling correctly on click
+$(document).ready(function(){
+    $(".dropdown-toggle").click(function(e){
+        var $el = $(this).next(".dropdown-menu");
+        var isVisible = $el.is(":visible");
+        
+        // Close all other dropdowns
+        $(".dropdown-menu").slideUp("400");
+        
+        // Toggle current dropdown
+        if (!isVisible) {
+            $el.stop(true, true).slideDown("400");
+        }
+    });
+
+    // Close the dropdown when clicking outside
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.dropdown').length) {
+            $(".dropdown-menu").slideUp("400");
+        }
+    });
+});
